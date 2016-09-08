8ab837388f31bc6e36b38c5c0f9c22eec6bc5922
Agustin Pivetta <aguspivetta@gmail.com>
2016-09-08 00:35:52 -0300

    initial

---
+++ client/app/components/about/about.controller.js
@@ -0,0 +1,13 @@
+class AboutController {
+  constructor() {
+    this.name = 'about';
+  	console.log('hola gato');
+  }
+
+  log() {
+  	console.log('hola gato');
+  }
+
+}
+
+export default AboutController;
---
+++ queries/caba points.sql
@@ -0,0 +1,4 @@
+SELECT encrypt_plate_id, longitude, latitude
+  FROM reports
+  WHERE encrypt_plate_id IN (SELECT DISTINCT a.encrypt_plate_id FROM reports as a, regions as b WHERE b.gid = 23 AND ST_Intersects(ST_SetSRID(ST_MakePoint(a.longitude,a.latitude),4326),b.geom))
+  ORDER BY encrypt_plate_id ASC, loc_time ASC;
